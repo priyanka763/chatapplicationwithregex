fb2bc9d546a2b01f977e84fedbbbe0cc128ab7b9
priyanka763 <priyanka.r@logicoy.com>
2019-06-13 19:01:41 +0530

    This is a chat application with quiz

---
+++ src/com/logicoy/chatapplication/Chatapplication.java
@@ -0,0 +1,15 @@
+
+package com.logicoy.chatapplication;
+
+import com.logicoy.chatapplication.gui.ChatScreen;
+
+
+public class Chatapplication {
+
+  public static void main(String args[]){
+   ChatScreen chat=new ChatScreen();
+   chat.show();
+  
+  }
+    
+}
